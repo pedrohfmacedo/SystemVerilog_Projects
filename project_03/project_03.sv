a344b4219fc007aa04c6f0e18b88ea4f59e900eb
Pedro Henrique Macêdo <101015333+pedrohfmacedo@users.noreply.github.com>
2023-11-29 14:15:45 -0300

    Update project_03.sv

--- project_03/project_03.sv
+++ project_03/project_03.sv
@@ -1,96 +1,52 @@
-module mef(
-  		   input logic E, CLK, CLR,
-  output Y[7:0]);
+//Neste projeto, foi utilizado a utlização de blocos menores de comparações, chamado "ordena_2", para organizarem os valores em grupos e realizar as comparações entre os resultados encontrados nestas conexões.
+module ordener2 (
+  input logic [7:0] a, b, c, d, e, f, g, h,
+    output logic [7:0] s0, s1, s2, s3, s4, s5, s6, s7
+);
 
- 			 parameter [7:0] S1 = 8'b00;
- 			 parameter [7:0] S2 = 8'b01;
- 			 parameter [7:0] S3 = 8'b10;
- 			 parameter [7:0] S4 = 8'b110;
- 			 parameter [7:0] S5 = 8'b111000;
- 			 parameter [7:0] S6 = 8'b111001;
-			 parameter [7:0] S7 = 8'b111010;
-       parameter [7:0] S8 = 8'b1110110;
-  		 parameter [7:0] S9 = 8'b1110111;
-			 parameter [7:0] S10 = 8'b1111000;
-		 	 parameter [7:0] S11 = 8'b1111001;
-  		 parameter [7:0] S12 = 8'b1111010;
-	  	 parameter [7:0] S13 = 8'b1111011;
- 			 parameter [7:0] S14 = 8'b1111100;
- 			 parameter [7:0] S15 = 8'b1111101;
- 			 parameter [7:0] S16 = 8'b1111110;
- 			 parameter [7:0] S17 = 8'b11111110;
-  		 parameter [7:0] S18 = 8'b11111111;
-
-  		parameter [4:0] code1 = 5'b000001;
-    	parameter [4:0] code2 = 5'b000010;
-     	parameter [4:0] code3 = 5'b000011;
-     	parameter [4:0] code4 = 5'b000100;
-    	parameter [4:0] code5 = 5'b000101;
-    	parameter [4:0] code6 = 5'b000110;
-      parameter [4:0] code7 = 5'b000111;
-      parameter [4:0] code8 = 5'b001000;
-      parameter [4:0] code9 = 5'b001001;
-      parameter [4:0] code10 = 5'b001010;
-      parameter [4:0] code11 = 5'b001011;
-      parameter [4:0] code12 = 5'b001100;
-      parameter [4:0] code13 = 5'b001101;
-      parameter [4:0] code14 = 5'b001110;
-      parameter [4:0] code15 = 5'b001111;
-      parameter [4:0] code16 = 5'b010000;
-      parameter [4:0] code17 = 5'b010001;
-      parameter [4:0] code17 = 5'b010010;
- 	 
-      logic [7:0] atual_estado, proximo_estado;
+  logic [7:0] h1, h2, h3, h4, h5, n1, n2, n3, n4;
+  logic [7:0] h10, h20, h30, h40, n10, n20, n30, n40;
+  logic [7:0] h100, h200, h300, h400, h500, h600, h700, h800;
   
-  //Lógica de próximo estado
-  always@(atual_estado or E)
-  	begin
-      case(atual_estado)
-        S1  : proximo_estado = E ? S1 : S2;
-        S2  : proximo_estado = E ? S1 : S3;
-        S3  : proximo_estado = E ? S2 : S3; 
-        S4  : proximo_estado = E ? S0 : S1;
-        S5  : proximo_estado = E ? S0 : S1;
-        S6  : proximo_estado = E ? S0 : S2;
-        S7  : proximo_estado = E ? S0 : S3; 
-        S8  : proximo_estado = E ? S0 : S1;
-        S9  : proximo_estado = E ? S0 : S1;
-        S10 : proximo_estado = E ? S0 : S2;
-        S11 : proximo_estado = E ? S0 : S3; 
-        S12 : proximo_estado = E ? S0 : S1;
-        S13 : proximo_estado = E ? S0 : S1;
-        S14 : proximo_estado = E ? S0 : S2;
-        S15 : proximo_estado = E ? S0 : S3; 
-        S16 : proximo_estado = E ? S0 : S1;
-        S17 : proximo_estado = E ? S0 : S1;
-        S18: proximo_estado = E ? S0 : S2;
-        default proximo_estado = 0;
-      endcase
-    end
-  //Atualização de registradores
-  always@(posedge CLK)
-    begin
-      atual_estado <= CLR ? 0: proximo_estado;
-    end
-  //Lógica de saída
-  assign Y = (atual_estado == code1 )? 1 :
-             (atual_estado == code2)? 2:
-             (atual_estado == code3)? 3:
-             (atual_estado == code4)? 4:
-             (atual_estado == code5)? 5:
-             (atual_estado == code6)? 6:
-             (atual_estado == code7)? 7:
-             (atual_estado == code8)? 8:
-             (atual_estado == code9)? 9:
-             (atual_estado == code10)? 10:
-             (atual_estado == code11)? 11:
-             (atual_estado == code12)? 12:
-             (atual_estado == code13)? 13:
-             (atual_estado == code14)? 14:
-             (atual_estado == code15)? 15:
-             (atual_estado == code16)? 16:
-             (atual_estado == code17)? 17:
-             (atual_estado == code18)? 18:0;
-     
+  ordena_2 inst1(.a(a), .b(b), .maior(h1), .menor(n1));
+  ordena_2 inst2(.a(c), .b(d), .maior(h2), .menor(n2));
+  ordena_2 inst3(.a(e), .b(f), .maior(h3), .menor(n3));
+  ordena_2 inst4(.a(g), .b(h), .maior(h4), .menor(n4));  
+  // 2 maiores números
+  ordena_2 inst5(.a(h1),  .b(h2),  .maior(h10),  .menor(n10));
+  ordena_2 inst6(.a(h3),  .b(h4),  .maior(h20),  .menor(n20));
+  ordena_2 aux01(.a(h10), .b(h20), .maior(h100), .menor(h200)); //h100 maior número e h200 é o segundo maior número
+  //2 menores números
+  ordena_2 inst7 (.a(n1), .b(n2), .maior(h30),   .menor(n30));
+  ordena_2 inst8 (.a(n3), .b(n4), .maior(h40),   .menor(n40));
+  ordena_2 aux02(.a(n30), .b(n40),.maior(h400),  .menor(h300)); //h300 menor númeor e h400 é o segundo menor
+  // meio
+  ordena_2 inst9 (.a(n10), .b(n20), .maior(h500), .menor(h600)); //
+  ordena_2 inst10(.a(h30), .b(h40), .maior(h700), .menor(h800));
+  
+  assign s0 = h300;
+  assign s1 = h400;	  
+  assign s2 = h800;
+  assign s3 = h700;
+  assign s4 = h600;
+  assign s5 = h500;
+  assign s6 = h200;
+  assign s7 = h100;
+
 endmodule
+
+//módulo ordena_2realiza a organização crescente entre 2 números.
+module ordena_2(input logic [7:0]a,b,
+                output logic [7:0]maior,menor);
   
+    always@(*) begin
+      if(a<b) begin
+              maior=b;
+      		  menor=a;
+      end
+              else begin
+              maior=a;
+      		  menor=b;
+      end
+   end
+endmodule
