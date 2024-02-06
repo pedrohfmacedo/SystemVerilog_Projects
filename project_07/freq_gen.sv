adfd73c130aadcfb969b6d455a747e2608e772bc
Pedro Henrique Macêdo <101015333+pedrohfmacedo@users.noreply.github.com>
2024-02-05 21:34:32 -0300

    Add files via upload

--- project_07/freq_gen.sv
+++ project_07/freq_gen.sv
@@ -1,33 +1,32 @@
+/*
+      This is a frequency generator with a parallel data input,
+      intended to generate the desired frequency
+*/
+
+// Set the timescale for the module
 `timescale 1ns/1ps
 
 module freq_gen #(
-  parameter DataWidth = 8)(
-  input logic clk_in,           
-  input logic [DataWidth-1:0] freq_sel,
-  input rst,
-  output logic clk_out
-);
+    int DataWidth = 8
+) (
+    input  logic clk_in, rst,
+    input  logic [DataWidth-1:0] freq_sel,
+    output logic clk_out);
   
+// Internal variable to hold the counter value
   logic [DataWidth:0] counter;
-  
-  
-  always@(posedge clk_in) begin
-    if(rst) begin
-		counter<= 1'b0;
-        clk_out<= 1'b0;
-    end
+
+// synchronous logic
+  always_ff @(posedge clk_in) begin
+    if (rst) begin
+        counter <= 1'b0;
+    end 
     else begin
-      counter<=counter+freq_sel+1;
-    end
-  end
-   
-  always@(*) begin
-    if (counter >=freq_sel)begin 
-      clk_out = counter[DataWidth];
-    end
-    else begin
-      clk_out = 0;
-    end
-  end
-  
+        counter <= counter + freq_sel + 1'b1;
+	end
+end
+
+//continuous assignment out
+assign clk_out = counter[DataWidth];
+
 endmodule
---
+++ project_07/waves.tcl
@@ -0,0 +1,4 @@
+database -vcd -open waves -into waves.vcd -default 
+probe -create -vcd -all -depth all
+run
+exit
